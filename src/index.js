d6526d8ad349a5385afcc97ed421ceaa13f9d42f
Gregory Anderson <anderson.gregory.mail@gmail.com>
2020-04-08 23:27:49 -0600

    Establish button look

--- src/index.js
+++ src/index.js
@@ -9,5 +9,8 @@ import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/Greg.png'
+
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
